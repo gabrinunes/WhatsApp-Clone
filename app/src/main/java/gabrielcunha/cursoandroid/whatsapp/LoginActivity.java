a0c4bb0258bdeb68dc8487d5337c83523b087ebe
Gabriel Cunha <gn_cunha@hotmail.com>
2019-10-03 11:16:02 -0300

    criação da interface de cadastro

--- app/src/main/java/gabrielcunha/cursoandroid/whatsapp/LoginActivity.java
+++ app/src/main/java/gabrielcunha/cursoandroid/whatsapp/LoginActivity.java
@@ -2,13 +2,29 @@ package gabrielcunha.cursoandroid.whatsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button buttonLogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        inicializarComponentes();
+    }
+
+    private void inicializarComponentes() {
+
+    }
+    public void abrirTelaCadastro(View view){
+        startActivity(new Intent(LoginActivity.this,CadastroActivity.class));
     }
 }
