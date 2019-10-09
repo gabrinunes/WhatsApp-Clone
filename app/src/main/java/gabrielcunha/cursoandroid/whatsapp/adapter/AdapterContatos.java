b6421d3ededb4b0a448ad4d8688cb703def817b9
Gabriel Cunha <gn_cunha@hotmail.com>
2019-10-09 13:34:38 -0300

    Listando contatos

---
+++ app/src/main/java/gabrielcunha/cursoandroid/whatsapp/adapter/AdapterContatos.java
@@ -0,0 +1,78 @@
+package gabrielcunha.cursoandroid.whatsapp.adapter;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import gabrielcunha.cursoandroid.whatsapp.R;
+import gabrielcunha.cursoandroid.whatsapp.model.Usuario;
+
+public class AdapterContatos extends RecyclerView.Adapter<AdapterContatos.MyViewHolder> {
+
+    private List<Usuario> usuarios;
+    private Context context;
+
+    public AdapterContatos(List<Usuario> usuarios,Context c){
+
+        this.usuarios = usuarios;
+        this.context = c;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_contatos,parent,false);
+        return new MyViewHolder(itemLista);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Usuario usuario = usuarios.get(position);
+        holder.nomePerfil.setText(usuario.getNome());
+        holder.Email.setText(usuario.getEmail());
+
+        //Carrega imagem
+
+        if(usuario.getFoto()!=null){
+            Uri uri = Uri.parse(usuario.getFoto());
+            Picasso.get()
+                    .load(uri)
+                    .into(holder.imagemPerfil);
+        }else{
+         holder.imagemPerfil.setImageResource(R.drawable.padrao);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return usuarios.size() ;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        CircleImageView imagemPerfil;
+        TextView nomePerfil;
+        TextView Email;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imagemPerfil = itemView.findViewById(R.id.imagePerfil);
+            nomePerfil = itemView.findViewById(R.id.NomeUsuario);
+            Email = itemView.findViewById(R.id.Email);
+        }
+    }
+    }
+
+
--- app/src/main/java/gabrielcunha/cursoandroid/whatsapp/fragment/ContatosFragment.java
+++ app/src/main/java/gabrielcunha/cursoandroid/whatsapp/fragment/ContatosFragment.java
@@ -4,21 +4,40 @@ package gabrielcunha.cursoandroid.whatsapp.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import gabrielcunha.cursoandroid.whatsapp.R;
+import gabrielcunha.cursoandroid.whatsapp.adapter.AdapterContatos;
+import gabrielcunha.cursoandroid.whatsapp.config.ConfiguracaoFirebase;
+import gabrielcunha.cursoandroid.whatsapp.model.Usuario;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ContatosFragment extends Fragment {
 
+    private RecyclerView recyclerContatos;
+    private AdapterContatos adapterContatos;
+    private List<Usuario> usuarios = new ArrayList<>();
+    private DatabaseReference usuarioRef;
+    private ValueEventListener valueEventListenerContatos;
+
 
     public ContatosFragment() {
-        // Required empty public constructor
+
     }
 
 
@@ -26,7 +45,59 @@ public class ContatosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false);
+        View view = inflater.inflate(R.layout.fragment_contatos, container, false);
+
+        recyclerContatos = view.findViewById(R.id.recyclerContatos);
+        usuarioRef = ConfiguracaoFirebase.getFirebaseDatabase().child("usuarios");
+
+
+        //Configurar adapter
+
+        adapterContatos = new AdapterContatos(usuarios,getActivity());
+
+        //Configurar RecyclerView
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());// Usar getActivity para pegar o context quando estiver usando um fragment
+        recyclerContatos.setLayoutManager(layoutManager);
+        recyclerContatos.setHasFixedSize(true);
+        recyclerContatos.setAdapter(adapterContatos);
+
+
+
+        return view;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        recuperaUsuarios();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        usuarioRef.removeEventListener(valueEventListenerContatos);
+    }
+
+    private void recuperaUsuarios() {
+
+        valueEventListenerContatos = usuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                 usuarios.clear();
+                for(DataSnapshot ds:dataSnapshot.getChildren()){
+                    Usuario usuario = ds.getValue(Usuario.class);
+                    usuarios.add(usuario);
+                }
+                adapterContatos.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 
 }
