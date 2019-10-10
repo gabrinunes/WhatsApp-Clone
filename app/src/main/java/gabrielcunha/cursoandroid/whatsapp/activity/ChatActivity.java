520b1276c025714d4046e2bf6c0ec8407c15692a
Gabriel Cunha <gn_cunha@hotmail.com>
2019-10-10 12:12:13 -0300

    Salvando Mensagem

--- app/src/main/java/gabrielcunha/cursoandroid/whatsapp/activity/ChatActivity.java
+++ app/src/main/java/gabrielcunha/cursoandroid/whatsapp/activity/ChatActivity.java
@@ -4,23 +4,35 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
 import com.squareup.picasso.Picasso;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import gabrielcunha.cursoandroid.whatsapp.R;
+import gabrielcunha.cursoandroid.whatsapp.config.ConfiguracaoFirebase;
+import gabrielcunha.cursoandroid.whatsapp.helper.Base64Custom;
+import gabrielcunha.cursoandroid.whatsapp.helper.UsuarioFirebase;
+import gabrielcunha.cursoandroid.whatsapp.model.Mensagem;
 import gabrielcunha.cursoandroid.whatsapp.model.Usuario;
 
 public class ChatActivity extends AppCompatActivity {
 
     private CircleImageView circleImageViewFoto;
     private TextView textViewNome;
+    private EditText editMensagem;
     private Usuario usuarioDestinatario;
+
+    //identificador usuarios remetente e destinatario
+    private String idUsuarioRemetente;
+    private String idUsuarioDestinatario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,21 +43,28 @@ public class ChatActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         inicializarComponentes();
 
+        //Recupera dados do usuario remetente
+        idUsuarioRemetente = UsuarioFirebase.getIdUsuario();
+
         //Recuperar dados do usuário destinatario
         Bundle bundle = getIntent().getExtras();
-        if(bundle!=null){
+        if (bundle != null) {
 
             usuarioDestinatario = (Usuario) bundle.getSerializable("chatContato");
             textViewNome.setText(usuarioDestinatario.getNome());
 
             String foto = usuarioDestinatario.getFoto();
-            if(foto!=null){
+            if (foto != null) {
                 Picasso.get()
                         .load(foto)
                         .into(circleImageViewFoto);
-            }else{
+            } else {
                 circleImageViewFoto.setImageResource(R.drawable.padrao);
             }
+
+            //recuperar dados usuario destinatario
+
+            idUsuarioDestinatario = Base64Custom.codificarBase64(usuarioDestinatario.getEmail());
         }
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -54,6 +73,46 @@ public class ChatActivity extends AppCompatActivity {
     private void inicializarComponentes() {
         circleImageViewFoto = findViewById(R.id.circleImageFotoChat);
         textViewNome = findViewById(R.id.textViewNomeChat);
+        editMensagem = findViewById(R.id.editMensagem);
+    }
+
+    private void exibirMensagem(String texto) {
+        Toast.makeText(this, texto, Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void enviarMensagem(View view) {
+
+        String mensagem = editMensagem.getText().toString();
+
+        if (!mensagem.isEmpty()) {
+
+            Mensagem msg = new Mensagem();
+            msg.setIdUsuario(idUsuarioRemetente);
+            msg.setMensagem(mensagem);
+
+            //Salvar a mensagem
+
+            salvarMensagem(idUsuarioRemetente, idUsuarioDestinatario, msg);
+
+        } else {
+            exibirMensagem("Digite uma mensagem para enviar!");
+        }
+    }
+
+    private void salvarMensagem(String idRemetente, String idDestinatario, Mensagem msg) {
+
+        DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebaseDatabase();
+        DatabaseReference mensagemRef = firebaseRef.child("mensagens");
+
+        mensagemRef.child(idRemetente)
+                .child(idDestinatario)
+                .push()
+                .setValue(msg);
+
+        //Limpar texto
+        editMensagem.setText("");
+
     }
 
 }
