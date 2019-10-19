4b495560a574cfd5e1d4e402ba7492bd3fb78e5e
Gabriel Cunha <gn_cunha@hotmail.com>
2019-10-19 12:49:42 -0300

    Criando interface de cadastro de grupo

--- app/src/main/java/gabrielcunha/cursoandroid/whatsapp/activity/CadastroGrupoActivity.java
+++ app/src/main/java/gabrielcunha/cursoandroid/whatsapp/activity/CadastroGrupoActivity.java
@@ -7,6 +7,8 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 import android.widget.TextView;
@@ -14,18 +16,27 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import gabrielcunha.cursoandroid.whatsapp.R;
+import gabrielcunha.cursoandroid.whatsapp.adapter.GrupoSelecionadoAdapter;
 import gabrielcunha.cursoandroid.whatsapp.model.Usuario;
 
 public class CadastroGrupoActivity extends AppCompatActivity {
 
     private List<Usuario> listaMembrosSelecionados = new ArrayList<>();
+    private TextView textTotalParticipantes;
+    private CircleImageView imageGrupo;
+    private GrupoSelecionadoAdapter grupoSelecionadoAdapter;
+    private RecyclerView recyclerMembrosSelecionados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_grupo);
+        inicializarComponentes();
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Novo Grupo");
+        toolbar.setSubtitle("Defina o nome");
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fabAvancarCadastro);
@@ -43,7 +54,26 @@ public class CadastroGrupoActivity extends AppCompatActivity {
             List<Usuario> membros = (List<Usuario>) getIntent().getExtras().getSerializable("membros");
             listaMembrosSelecionados.addAll(membros);
 
+            textTotalParticipantes.setText("Participantes: " + listaMembrosSelecionados.size());
+
         }
+
+        //Configurar o RecyclerView
+        grupoSelecionadoAdapter = new GrupoSelecionadoAdapter(listaMembrosSelecionados, getApplicationContext());
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext()
+                , LinearLayoutManager.HORIZONTAL, false);
+        recyclerMembrosSelecionados.setLayoutManager(layoutManager);
+        recyclerMembrosSelecionados.setHasFixedSize(true);
+        recyclerMembrosSelecionados.setAdapter(grupoSelecionadoAdapter);
+
+
+    }
+
+    private void inicializarComponentes() {
+        textTotalParticipantes = findViewById(R.id.textTotalParticipantes);
+        imageGrupo = findViewById(R.id.imageGrupo);
+        recyclerMembrosSelecionados = findViewById(R.id.recyclerMembrosGrupo);
+
     }
 
 }
